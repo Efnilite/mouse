b58224471f417b006c1f4cdc9d081aa843ab7ca8
Efnilite <35348263+Efnilite@users.noreply.github.com>
2025-10-03 15:57:32 +0200

    Add path optimizing

--- src/path.rs
+++ src/path.rs
@@ -1,6 +1,6 @@
-use crate::maze::Segment;
-use crate::vec::Veci;
 use crate::MAZE_SIZE;
+use crate::vec::Veci;
+use std::collections::HashMap;
 use std::fmt;
 
 /// Represents a path that may be taken
@@ -26,7 +26,7 @@ impl Path {
     /// If the segment has not been visited yet, returns [Segment::new].
     pub fn segment(&self, index: usize) -> Option<Veci> {
         if index >= self.size() {
-            return None
+            return None;
         }
         Some(self.segments[index])
     }
@@ -34,7 +34,7 @@ impl Path {
     /// Returns the current head of the path.
     pub fn head(&self) -> Option<Veci> {
         if self.size() == 0 {
-            return None
+            return None;
         }
         Some(self.segments[self.size() - 1])
     }
@@ -58,6 +58,45 @@ impl Path {
             self.segments.push(*segment)
         }
     }
+
+    /// Returns whether this path contains the specified vector.
+    ///
+    /// ### Arguments
+    ///
+    /// - `vec` - The vec to check for containment.
+    pub fn contains(&self, vec: Veci) -> bool {
+        for i in (0..self.size()).rev() {
+            if self.segments[i] == vec {
+                return true;
+            }
+        }
+        false
+    }
+
+    /// Optimizes this path by removing any cycle that has the same start and end point.
+    /// Assigns `self.segments` to a new optimized [Vec].
+    pub fn optimize(&mut self) {
+        // the first found position of every veci
+        let mut occurrences = HashMap::new();
+        let mut optimized = Vec::with_capacity(self.size());
+
+        for (i, pos) in self.segments.iter().enumerate() {
+            let existing = occurrences.insert(*pos, i);
+
+            if existing.is_none() {
+                optimized.push(*pos);
+                continue;
+            }
+
+            let existing = existing.unwrap();
+            for _ in (existing..i).rev() {
+                optimized.pop();
+            }
+            optimized.push(*pos);
+        }
+
+        self.segments = optimized;
+    }
 }
 
 impl fmt::Debug for Path {
@@ -80,7 +119,30 @@ mod tests {
         let mut path = Path::new();
 
         path.append(Veci::new());
-        assert_eq!(2, path.size());
-        assert_eq!(Segment::new().pos(), path.segment(0).expect("Failed to find first path segment"));
+        assert_eq!(1, path.size());
+        assert_eq!(
+            Segment::new().pos(),
+            path.segment(0).expect("Failed to find first path segment")
+        );
+    }
+
+    #[test]
+    fn optimize() {
+        let mut path = Path::new();
+
+        path.append(Veci { x: 0, y: 0});
+        path.append(Veci { x: 1, y: 0});
+        path.append(Veci { x: 0, y: 1});
+        path.append(Veci { x: 1, y: 0});
+        path.append(Veci { x: 3, y: 1});
+        path.append(Veci { x: 1, y: 0});
+        path.append(Veci { x: 2, y: 0});
+
+        path.optimize();
+
+        assert_eq!(3, path.size());
+        assert_eq!(Veci { x: 0, y: 0 },  path.segment(0).unwrap());
+        assert_eq!(Veci { x: 1, y: 0 },  path.segment(1).unwrap());
+        assert_eq!(Veci { x: 2, y: 0 },  path.segment(2).unwrap());
     }
 }

9c1739f6a220ab29eed8aa569e39f1850c1c70c0
Efnilite <35348263+Efnilite@users.noreply.github.com>
2025-11-04 16:49:15 +0100

    Add second loop to main

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use mouse::maze::Maze;
 use mouse::path::Path;
 use mouse::pathfinder;
+use mouse::pathfinder::Target;
 use mouse::vec::{Vecf, Vecu};
 
 fn main() {
@@ -20,15 +21,12 @@ fn main() {
     maze.update_walls(1, 1, [false, false, true, true]);
     maze.update_walls(2, 1, [true, false, true, false]);
     maze.update_walls(3, 1, [false, true, true, false]);
+    maze.update_walls(7, 7, [true, false, false, true]);
+    maze.update_walls(8, 7, [true, true, false, false]);
 
     // first deep dive
     loop {
-        let result = pathfinder::next(
-            &maze,
-            &first,
-            |a, b| a.distance < b.distance,
-            |a, b| a.distance <= b.distance,
-        );
+        let result = pathfinder::next(&maze, &first, Target::Center);
 
         match result {
             pathfinder::Result::Found(next) => {
@@ -37,27 +35,29 @@ fn main() {
                 if next.distance == 0 {
                     break;
                 }
-                continue;
             }
             pathfinder::Result::Stuck(next) => {
                 first.append_all(&next);
                 pathfinder::update_distances(&mut maze, &first);
             }
         }
+        println!("{:?}", first);
     }
 
     // todo find first unvisited node
     first.optimize();
+    println!("{:?}", first);
     let mut second = pathfinder::nearest_unvisited(&maze, &first);
+
+    println!("{:?}", second);
 
     // second
     loop {
-        let result = pathfinder::next(
-            &maze,
-            &second,
-            |a, b| a.distance > b.distance,
-            |a, b| a.distance >= b.distance,
-        );
+        let result = pathfinder::next(&maze, &second, Target::Origin);
+
+        if second.len() % 10 == 0 {
+            println!("{:?}", second);
+        }
 
         match result {
             pathfinder::Result::Found(next) => {
@@ -66,7 +66,6 @@ fn main() {
                 if next.pos() == Vecu::new() {
                     break;
                 }
-                continue;
             }
             pathfinder::Result::Stuck(next) => {
                 second.append_all(&next);
@@ -75,7 +74,8 @@ fn main() {
         }
     }
 
-    println!("{:?}", first);
-    println!("{:?}", second);
+    second.optimize();
+
+    // println!("{:?}", second);
     println!("{:?}", maze);
 }
